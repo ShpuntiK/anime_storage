37a628ced6755a68ac538643a09156f32223da49
Alexander Ivantsov <shpuntik37@ya.ru>
2014-09-27 01:32:01 +0600

    add directive for navbar

---
+++ static/js/app/directives/navbar-active.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('main').directive('navbarActive', ['$location', function ($location) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            scope.$watch(function() {
+                return $location.path();
+            }, function(value){
+                var regexp = new RegExp('^' + value + '$');
+
+                angular.forEach(element.children(), function(li) {
+                    var link = li.lastChild.hash.replace('#', ''),
+                        isMatched = regexp.test(link);
+
+                    li.className = isMatched ? 'active' : '';
+                });
+            });
+        }
+    }
+}]);
