ad74c6742f596c65a150fd7b84dc63c3e0a40083
Alexander Ivantsov <shpuntik37@ya.ru>
2014-12-20 00:34:41 +0600

    add usemin grunt task, update grunt configuration, update template

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,29 @@ module.exports = function (grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         watch: {
-            files: ['./static/scripts/**/*', './static/styles/**/*'],
-            tasks: ['newer:less', 'newer:ngAnnotate']
+            files: ['static/styles/**/*'],
+            tasks: ['newer:less']
+        },
+
+        less: {
+            options: {
+                paths: ['static/styles/less']
+            },
+            files: {
+                expand: true,
+                cwd: 'static/styles/less',
+                src: ['main.less'],
+                dest: 'static/styles',
+                ext: '.css'
+            }
+        },
+
+        autoprefixer: {
+            styles: {
+                files: {
+                    'static/styles/main.css': 'static/styles/main.css'
+                }
+            }
         },
 
         jshint: {
@@ -12,25 +33,9 @@ module.exports = function (grunt) {
                 jshintrc: '.jshintrc'
             },
             files: [
-                './static/scripts/**/*.js',
-                './tests/client/unit/**/*.js'
+                'static/scripts/**/*.js',
+                'tests/client/unit/**/*.js'
             ]
-        },
-
-        less: {
-            options: {
-                paths: ['./static/styles'],
-                sourceMap: true,
-                sourceMapFilename: './static/_/css/main.css.map',
-                sourceMapURL: '/static/_/css/main.css.map'
-            },
-            files: {
-                expand: true,
-                cwd: './static/styles',
-                src: ['main.less'],
-                dest: './static/_/css',
-                ext: '.css'
-            }
         },
 
         ngAnnotate: {
@@ -39,27 +44,55 @@ module.exports = function (grunt) {
             },
             files: {
                 expand: true,
-                cwd: './static/scripts',
-                src: ['**/*.js'],
-                dest: './static/_/js'
+                cwd: '.tmp/concat/static/scripts',
+                src: ['app.js'],
+                dest: '.tmp/concat/static/scripts'
             }
         },
 
-        compress: {
+        useminPrepare: {
+            html: ['dist/templates/index.html'],
             options: {
-                archive: 'deploy.zip'
-            },
+                root: '.'
+            }
+        },
+
+        usemin: {
+            html: ['dist/templates/index.html'],
+            options: {
+                assetsDirs: ['dist']
+            }
+        },
+
+        filerev: {
             files: {
-                expand: true,
-                cwd: './',
-                src: ['./anime_storage/**/*.py',
-                    './apps/**/*.py',
-                    './static/**/*',
-                    './templates/**/*',
-                    './configs/**/*',
-                    'manage.py',
-                    'requirements.txt'
+                src: [
+                    'dist/static/scripts/*.js',
+                    'dist/static/styles/main.css'
                 ]
+            }
+        },
+
+        clean: {
+            dist: ['dist'],
+            tmp: ['.tmp']
+        },
+
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.',
+                    src: [
+                        'anime_storage/**/*.py',
+                        'apps/**/*.py',
+                        'templates/**/*',
+                        'configs/**/*',
+                        'manage.py',
+                        'requirements.txt'
+                    ],
+                    dest: 'dist'
+                }]
             }
         },
 
@@ -70,6 +103,17 @@ module.exports = function (grunt) {
                     singleRun: true
                 }
             }
+        },
+
+        compress: {
+            options: {
+                archive: 'deploy.zip'
+            },
+            files: {
+                expand: true,
+                cwd: '',
+                src: ['dist']
+            }
         }
     });
 
@@ -77,10 +121,34 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-ng-annotate');
+    grunt.loadNpmTasks('grunt-filerev');
     grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-autoprefixer');
 
-    grunt.registerTask('build', ['less', 'jshint', 'ngAnnotate']);
+
+    grunt.registerTask('build', [
+        'jshint',
+        'clean',
+        'less',
+        'autoprefixer',
+        'copy',
+        'useminPrepare',
+        'concat',
+        'ngAnnotate',
+        'uglify',
+        'cssmin',
+        'filerev',
+        'usemin',
+        //'ngtemplate',
+        'clean:tmp'
+    ]);
     grunt.registerTask('zip', ['build', 'compress']);
 };
