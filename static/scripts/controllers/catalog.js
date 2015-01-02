853865a6dc7b3469ddb602663fdfe89e91c061f1
Alexander Ivantsov <shpuntik37@ya.ru>
2015-01-02 16:25:26 +0600

    make massonry anime catalog

--- static/scripts/controllers/catalog.js
+++ static/scripts/controllers/catalog.js
@@ -7,26 +7,16 @@
 
     function CatalogController($log, Anime) {
         var vm = this;
-        vm.animeRows = [];
+        vm.animes = [];
 
         getAnimes();
 
         function getAnimes() {
             Anime.query(function (res) {
-                vm.animeRows = splitArrayOnParts(res, 3);
+                vm.animes = res;
             }, function (err) {
                 $log.error(err);
             });
-        }
-
-        function splitArrayOnParts(arr, partSize) {
-            var splittedArray = [];
-
-            for (var i = 0, length = arr.length; i < length; i += partSize) {
-                splittedArray.push(arr.slice(i, i + partSize));
-            }
-
-            return splittedArray;
         }
     }
 })();
